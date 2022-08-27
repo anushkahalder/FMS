147cff918902f622d8098988183cdf165cb46367
anushkahalder <anushkahalder2@gmail.com>
2022-08-27 15:58:50 +0530

    Final pdt

---
+++ FinanceMangementSystem/src/main/java/com/example/finance/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.finance.controller;
+
+
+import com.example.finance.entity.Product;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public interface ProductController {
+
+}
--- FinanceMangementSystem/src/main/java/com/example/finance/repository/ProductRepository.java
+++ FinanceMangementSystem/src/main/java/com/example/finance/repository/ProductRepository.java
@@ -1,13 +1,31 @@
-/*
+
 package com.example.finance.repository;
 
+import com.example.finance.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.finance.entity.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface ProductRepository  extends JpaRepository<Product, Integer> {
 
+
+    
+
+
+    @Autowired
+    private ProductService pdtservide;
+
+
+    @PostMapping("/products")
+    public Product saveProduct(@RequestBody Product product){
+
+
+    }
+
 }
-*/
+
