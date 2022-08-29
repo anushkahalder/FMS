7ff3a163ce3621d8427105f23ecf3ac58c9cc66d
anushkahalder <anushkahalder2@gmail.com>
2022-08-29 09:40:37 +0530

    deleted packages

--- FinanceMangementSystem/src/main/java/com/example/finance/service/CardDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.finance.service;
-
-import com.example.finance.entity.CardDetails;
-
-public interface CardDetailsService {
-
-	public CardDetails saveCard(CardDetails card);
-
-}
--- FinanceMangementSystem/src/main/java/com/example/finance/service/EmiCardService.java
+++
@@ -1,9 +0,0 @@
-package com.example.finance.service;
-
-import com.example.finance.entity.EmiCard;
-
-public interface EmiCardService {
-
-	public EmiCard saveCard(EmiCard ecard);
-
-}
